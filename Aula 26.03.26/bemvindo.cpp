a41234ffc078126bac9f34841a832cff1a9ef805
PauloHenriqueCoelho <paulocoelhogonca@gmail.com>
2026-03-26 14:59:49 -0300

    Add files via upload

---
+++ Aula 26.03.26/bemvindo.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Paulo Henrique Goncalves Coelho on 26/03/26.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Bem-Vindo C++" << endl;
+    return 0;
+}
---
+++ Aula 26.03.26/nome.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Paulo Henrique Goncalves Coelho on 26/03/26.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Paulo Henrique Goncalves Coelho" << endl;
+    return 0;
+}
